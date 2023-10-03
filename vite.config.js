330136fc19197381fbfea3327da3362f4b4392a7
Nekekys <geri_337@mail.ru>
2023-10-03 20:36:51 +0300

    base change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/TestVueJokesSearch/',
+  base: '/TestVueJokesSearch/',
   plugins: [vue()],
 })
